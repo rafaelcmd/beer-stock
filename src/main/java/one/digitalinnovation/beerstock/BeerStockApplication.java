57f75d4ec12b00ebca10a446962c4307edf22d74
rafael <rafaelcmd@gmail.com>
2020-12-15 11:50:21 -0300

    Created API REST and unit test for created beer with Mockito

---
+++ src/main/java/one/digitalinnovation/beerstock/BeerStockApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStockApplication.class, args);
+	}
+
+}
